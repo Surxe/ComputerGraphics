39e678e529be34d466b0fcf3f2c6fd960a4bda2c
Ethan <119145352+Surxe@users.noreply.github.com>
2025-02-13 13:40:18 -0500

    Streamline add_point using limbo vertices/shapes

--- HW6/Split/js/paint_program.js
+++ HW6/Split/js/paint_program.js
@@ -3,16 +3,17 @@ class PaintProgram {
         this.program = program
         this.draw_mode = "triangle";
         this.shapes = [];
-        this.vertices = [];
+        this.next_vertices = [];
+        this.next_shape;
         this.args_per_vertex = 5; // 2 for position, 3 for color
     }
 
     get_num_vertices_current_shape() {
-        return this.vertices.length/this.args_per_vertex % 3
+        return this.next_vertices.length/this.args_per_vertex % 3
     }
 
     add_point(x, y, r, g, b, push=true) {
-        this.vertices.push(x, y, r, g, b);
+        this.next_vertices.push(x, y, r, g, b);
 
         var draw_mode;
         var num_vertices_current_shape = this.get_num_vertices_current_shape()
@@ -23,30 +24,25 @@ class PaintProgram {
             draw_mode = gl.LINE_LOOP
         }
         else if (num_vertices_current_shape == 1) {
-            if (!push) {
-                this.remove_last_vertex();
-            }
-            return;
+            draw_mode = gl.POINTS
         }
 
-        var shape = new Shape(this.vertices, draw_mode);
-        this.shapes.push(shape);
-        //shape.render(this.program);
+        this.next_shape = new Shape(this.next_vertices, draw_mode);
+        
         this.render();
-        if (!push) {
+
+        if (!push) { //after previewing the shape, remove the last vertex
             this.remove_last_vertex();
-            // Remove the last shape
-            this.shapes.pop();
-            return
         }
-        if (num_vertices_current_shape == 0) {
-            this.vertices = []; // clear the vertices after a triangle is drawn and pushed
-        }   
+        else if (num_vertices_current_shape == 0) { // if shape is complete
+            this.shapes.push(this.next_shape);
+            this.next_vertices = [];
+        }
     }
 
     remove_last_vertex() {
-        if (this.vertices.length > 0) {
-            this.vertices = this.vertices.slice(0, -this.args_per_vertex);
+        if (this.next_vertices.length > 0) {
+            this.next_vertices = this.next_vertices.slice(0, -this.args_per_vertex);
         }
         else {
             throw Exception("No vertices to remove")
@@ -56,14 +52,20 @@ class PaintProgram {
     del_current_shape() {
         var num_vertices_current_shape = this.get_num_vertices_current_shape()
         if (num_vertices_current_shape > 0) {
-            this.vertices = [];
+            this.next_vertices = [];
         }
         this.render();
     }
 
     render() {
+        // Render all existing shapes
         for (var i = 0; i < this.shapes.length; i++) {
             this.shapes[i].render(this.program);
+        }
+
+        // Preview the next shape
+        if (this.next_shape) {
+            this.next_shape.render(this.program);
         }
     }
 }
