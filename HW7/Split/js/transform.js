ccc540a8738a87c006ea24b22b797ceabaa6a602
Ethan <119145352+Surxe@users.noreply.github.com>
2025-03-10 11:41:40 -0400

    transform class cleaned up

--- HW7/Split/js/transform.js
+++ HW7/Split/js/transform.js
@@ -1,48 +1,45 @@
-class transform
-{
-    constructor()
-    {
+class Transform {
+    constructor() {
         this.forward = [0,0,1];
         this.right = [1,0,0];
         this.up = [0,1,0];
     }
 
-    doRotations(RotAngles)
-    {
-        this.xRot = [
+    rotate(degrees) {
+        this.x_rotation = [
                     [1,0,0,0],
-                    [0,Math.cos(RotAngles[0]),-1*Math.sin(RotAngles[0]),0],
-                    [0,Math.sin(RotAngles[0]),Math.cos(RotAngles[0]),0],
+                    [0,Math.cos(degrees[0]),-1*Math.sin(degrees[0]),0],
+                    [0,Math.sin(degrees[0]),Math.cos(degrees[0]),0],
                     [0,0,0,1]
                 ];		
-        this.yRot = [
-                [Math.cos(RotAngles[1]),0,Math.sin(RotAngles[1]),0],
+        this.y_rotation = [
+                [Math.cos(degrees[1]),0,Math.sin(degrees[1]),0],
                 [0,1,0,0],
-                [-1*Math.sin(RotAngles[1]),0,Math.cos(RotAngles[1]),0],
+                [-1*Math.sin(degrees[1]),0,Math.cos(degrees[1]),0],
                 [0,0,0,1]	
                 ];
-        this.zRot = [
-                    [Math.cos(RotAngles[2]),-1*Math.sin(RotAngles[2]),0,0],
-                    [Math.sin(RotAngles[2]),Math.cos(RotAngles[2]),0,0],
+        this.z_rotation = [
+                    [Math.cos(degrees[2]),-1*Math.sin(degrees[2]),0,0],
+                    [Math.sin(degrees[2]),Math.cos(degrees[2]),0,0],
                     [0,0,1,0],
                     [0,0,0,1]
                 ]
-        //this.forward = this.crossMultiply(xRot,[0,0,1,0]);		
-        this.forward = this.crossMultiply(this.zRot,this.crossMultiply(this.yRot,this.crossMultiply(this.xRot,[0,0,1,0])))
-        this.right = this.crossMultiply(this.zRot,this.crossMultiply(this.yRot,this.crossMultiply(this.xRot,[1,0,0,0])))
-        this.up = this.crossMultiply(this.zRot,this.crossMultiply(this.yRot,this.crossMultiply(this.xRot,[0,1,0,0])))
-    }			
-    crossMultiply(M,V)
-    {
-    console.log(M[0][3]);
-    console.log(V[3]);
-    var temp = [
-                M[0][0]*V[0]+M[0][1]*V[1]+M[0][2] * V[2]+ M[0][3]*V[3],
-                M[1][0]*V[0]+M[1][1]*V[1]+M[1][2] * V[2]+ M[1][3]*V[3],
-                M[2][0]*V[0]+M[2][1]*V[1]+M[2][2] * V[2]+ M[2][3]*V[3],
-                M[3][0]*V[0]+M[3][1]*V[1]+M[3][2] * V[2]+ M[3][3]*V[3]
-                ]
-    console.log(temp);
+        //this.forward = this.cross_multiply(x_rotation,[0,0,1,0]);		
+        this.forward = this.cross_multiply(this.z_rotation,this.cross_multiply(this.y_rotation,this.cross_multiply(this.x_rotation,[0,0,1,0])))
+        this.right = this.cross_multiply(this.z_rotation,this.cross_multiply(this.y_rotation,this.cross_multiply(this.x_rotation,[1,0,0,0])))
+        this.up = this.cross_multiply(this.z_rotation,this.cross_multiply(this.y_rotation,this.cross_multiply(this.x_rotation,[0,1,0,0])))
+    }		
+    	
+    cross_multiply(M,V) {
+        console.log(M[0][3]);
+        console.log(V[3]);
+        var temp = [
+                    M[0][0]*V[0]+M[0][1]*V[1]+M[0][2] * V[2]+ M[0][3]*V[3],
+                    M[1][0]*V[0]+M[1][1]*V[1]+M[1][2] * V[2]+ M[1][3]*V[3],
+                    M[2][0]*V[0]+M[2][1]*V[1]+M[2][2] * V[2]+ M[2][3]*V[3],
+                    M[3][0]*V[0]+M[3][1]*V[1]+M[3][2] * V[2]+ M[3][3]*V[3]
+                    ]
+        console.log(temp);
         return temp;
     }
     
