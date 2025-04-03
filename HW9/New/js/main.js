e1a6277b173baac4a5b87b629f86fa98ffd1255f
Surxe <119145352+Surxe@users.noreply.github.com>
2025-04-03 18:00:44 -0400

    rainbow octahedron

--- HW9/New/js/main.js
+++ HW9/New/js/main.js
@@ -4,32 +4,47 @@ const [gl, program, canvas] = GLSetup.init("glcanvas")
 const camera = new Camera();
 const game_engine = new GameEngine(camera);
 
+const vertices = [
+    // Top vertex
+    0,  1,  0,  
+    // Side vertices
+    1,  0,  0,  
+    0,  0,  1,  
+    -1,  0,  0,  
+    0,  0, -1,  
+    // Bottom vertex
+    0, -1,  0   
+    ];
+  
+  
+const indices = [
+    0, 1, 2,  // Top front-right
+    0, 2, 3,  // Top front-left
+    0, 3, 4,  // Top back-left
+    0, 4, 1,  // Top back-right
+    5, 2, 1,  // Bottom front-right
+    5, 3, 2,  // Bottom front-left
+    5, 4, 3,  // Bottom back-left
+    5, 1, 4   // Bottom back-right
+    ];
+  
+const colors = [
+    1, 0, 0,  // Red (Top front-right)
+    0, 1, 0,  // Green (Top front-left)
+    0, 0, 1,  // Blue (Top back-left)
+    1, 1, 0,  // Yellow (Top back-right)
+    1, 0, 1,  // Magenta (Bottom front-right)
+    0, 1, 1,  // Cyan (Bottom front-left)
+    1, 1, 1,  // White (Bottom back-left)
+    0, 0, 0   // Black (Bottom back-right)
+    ];
+      
 const e1 = new Entity(
-    [ -.5, 0.5, -2.0, 
-     -0.5, -0.5, -2.0, 
-      0.5, -0.5, -2.0 ,
-     
-     .5, .5, -2,
-    ], // Orange Triangle
-    [0, 1, 2, 0, 2, 3],
-
-    [ 1.0, 0.5, 0.0, 1.0, 0.5, 0.0, 1.0, 0.5, 0.0 ,
-        1.0, 0.5, 0.0, 1.0, 0.5, 0.0, 1.0, 0.5, 0.0
-    ],
+    [...vertices], [...indices], [...colors],
     [0, 0, 0],
 )
 const e2 = new Entity(
-    [ -.5, 0.5, -2.0, 
-        -0.5, -0.5, -2.0, 
-         0.5, -0.5, -2.0 ,
-        
-        .5, .5, -2,
-       ], // Orange Triangle
-       [0, 1, 2, 0, 2, 3],
-       
-       [ 1.0, 0, 0.0, 1.0, 0, 0.0, 1.0, 0, 0.0 ,
-           1.0, 0, 0.0, 1.0, 0, 0.0, 1.0, 0, 0.0
-       ],
+    [...vertices], [...indices], [...colors],
     [3, 0, 0],
 )
 
