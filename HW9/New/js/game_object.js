df2c367722e657ffed16e38ee43ca778e71494c0
Surxe <119145352+Surxe@users.noreply.github.com>
2025-04-03 20:18:19 -0400

    globalize GameObject vertices after movement instead of before

--- HW9/New/js/game_object.js
+++ HW9/New/js/game_object.js
@@ -1,33 +1,37 @@
 class GameObject {
     constructor(local_vertices, location=[0, 0, 0]) {
-        // globalize the vertices
-        for (let i = 0; i < local_vertices.length; i += 3) {
-            local_vertices[i] += location[0];
-            local_vertices[i + 1] += location[1];
-            local_vertices[i + 2] += location[2];
-        }
-        this.vertices = local_vertices;
-
+        this.local_vertices = local_vertices; // local vertices relative to the object
         this.location = location;
+        this.globalize_vertices();
+    }
+
+    globalize_vertices() {
+        // Get the global location of the object by adding entity's location to its local vertices
+        const globalized_verts = this.local_vertices.map((v, i) => {
+            if (i % 3 === 0) { // x coordinate
+                return v + this.location[0];
+            } else if (i % 3 === 1) { // y coordinate
+                return v + this.location[1];
+            } else { // z coordinate
+                return v + this.location[2];
+            }
+        });
+
+        this.vertices = globalized_verts;
     }
 
     get_next_position(position_velocities) {
         var new_vertices = [];
-        for (let i = 0; i < this.vertices.length; i += 3) {
-            new_vertices.push(this.vertices[i] + position_velocities[0]);
-            new_vertices.push(this.vertices[i + 1] + position_velocities[1]);
-            new_vertices.push(this.vertices[i + 2] + position_velocities[2]);
+        for (let i = 0; i < this.local_vertices.length; i += 3) {
+            new_vertices.push(this.local_vertices[i] + position_velocities[0]);
+            new_vertices.push(this.local_vertices[i + 1] + position_velocities[1]);
+            new_vertices.push(this.local_vertices[i + 2] + position_velocities[2]);
         }
         return new_vertices;
     }
 
     move(position_velocities, rotation_velocities) {
-        var new_vertices = [];
-        for (let i = 0; i < this.vertices.length; i += 3) {
-            new_vertices.push(this.vertices[i] + position_velocities[0]);
-            new_vertices.push(this.vertices[i + 1] + position_velocities[1]);
-            new_vertices.push(this.vertices[i + 2] + position_velocities[2]);
-        }
-        this.vertices = new_vertices;
+        this.local_vertices = this.get_next_position(position_velocities, rotation_velocities);
+        this.globalize_vertices();
     }
 }
