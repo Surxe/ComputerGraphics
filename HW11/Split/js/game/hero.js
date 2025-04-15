be9af2ab8d86feb9a574cfaf4e6468fe0784b14f
Ethan <119145352+Surxe@users.noreply.github.com>
2025-04-15 13:37:40 -0400

    amend

---
+++ HW11/Split/js/game/hero.js
@@ -0,0 +1,18 @@
+class Hero extends CameraActor {
+    constructor(...args) {
+        const health = 5;
+        super(...args); // Call the parent constructor
+        this.health = health; // Initial health of the character
+    }
+
+    // this received a hit of damage damage
+    hit_received(damage) {
+        this.health -= damage; 
+        if (this.health <= 0) {
+            this.should_destroy = true; // die
+        }
+        else {
+            console.log("Remaining health: " + this.health); // Log remaining health
+        }
+    }
+}
